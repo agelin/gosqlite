f66d2488b787d44c4f82eb4a95abd3bf51b60f04
Richard Lyman <richard.lyman@gmail.com>
2013-04-26 17:48:27 -0600

    .

--- sqlite/sqlite_test.go
+++ sqlite/sqlite_test.go
@@ -11,3 +11,50 @@ func TestOpenMemory(t *testing.T) {
   }
 }
 
+func TestCreateTableInMemory(t *testing.T) {
+	db, open_err := Open(":memory:")
+  if open_err != nil {
+    t.Error("Failed to open in memory only database")
+  }
+  create_error := db.Exec("CREATE TABLE test (col)")
+  if create_error != nil {
+    t.Error("Failed to create in-memory only table")
+  }
+}
+
+func TestInsertInMemory(t *testing.T) {
+	db, open_err := Open(":memory:")
+  if open_err != nil {
+    t.Error("Failed to open in memory only database")
+  }
+  create_error := db.Exec("CREATE TABLE test (col)")
+  if create_error != nil {
+    t.Error("Failed to create in-memory only table")
+  }
+  insert_error := db.Exec("INSERT INTO test VALUES ('value')")
+  if insert_error != nil {
+    t.Error("Failed to insert into memory only database")
+  }
+}
+
+func TestExecFirstAsString(t *testing.T) {
+	db, open_err := Open(":memory:")
+  if open_err != nil {
+    t.Error("Failed to open in memory only database")
+  }
+  create_error := db.Exec("CREATE TABLE test (col)")
+  if create_error != nil {
+    t.Error("Failed to create in-memory only table")
+  }
+  insert_error := db.Exec("INSERT INTO test VALUES ('value')")
+  if insert_error != nil {
+    t.Error("Failed to insert into memory only database")
+  }
+  result, err := db.ExecFirstAsString("SELECT * FROM test")
+  if err != nil {
+    t.Error("Failed to select from test")
+  }
+  if result != "value" {
+    t.Error("Failed to exec first as string")
+  }
+}
