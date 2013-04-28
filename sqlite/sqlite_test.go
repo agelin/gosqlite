47a47e27d4ed5de9ba38524f3242a8bcee21a881
Richard Lyman <richard.lyman@gmail.com>
2013-04-28 10:13:14 -0600

    .

--- sqlite/sqlite_test.go
+++ sqlite/sqlite_test.go
@@ -4,57 +4,45 @@ import (
 	"testing"
 )
 
-func TestOpenMemory(t *testing.T) {
-	_, open_err := Open(":memory:")
-  if open_err != nil {
-    t.Error("Failed to open in memory only database")
+func open(t *testing.T) *Conn {
+  db, err := Open(":memory:")
+  if err != nil {
+    t.Error("Failed to open memory database")
+  }
+  return db
+}
+
+func (db *Conn) create(t *testing.T, sql string) {
+  err := db.Exec(sql)
+  if err != nil {
+    t.Error("Failed to create")
   }
 }
 
-func TestCreateTableInMemory(t *testing.T) {
-	db, open_err := Open(":memory:")
-  if open_err != nil {
-    t.Error("Failed to open in memory only database")
-  }
-  create_error := db.Exec("CREATE TABLE test (col)")
-  if create_error != nil {
-    t.Error("Failed to create in-memory only table")
-  }
-}
-
-func TestInsertInMemory(t *testing.T) {
-	db, open_err := Open(":memory:")
-  if open_err != nil {
-    t.Error("Failed to open in memory only database")
-  }
-  create_error := db.Exec("CREATE TABLE test (col)")
-  if create_error != nil {
-    t.Error("Failed to create in-memory only table")
-  }
-  insert_error := db.Exec("INSERT INTO test VALUES ('value')")
-  if insert_error != nil {
-    t.Error("Failed to insert into memory only database")
+func (db *Conn) insert(t *testing.T, sql string) {
+  err := db.Exec(sql)
+  if err != nil {
+    t.Error("Failed to insert")
   }
 }
 
 func TestExecFirstAsString(t *testing.T) {
-	db, open_err := Open(":memory:")
-  if open_err != nil {
-    t.Error("Failed to open in memory only database")
-  }
-  create_error := db.Exec("CREATE TABLE test (col)")
-  if create_error != nil {
-    t.Error("Failed to create in-memory only table")
-  }
-  insert_error := db.Exec("INSERT INTO test VALUES ('value')")
-  if insert_error != nil {
-    t.Error("Failed to insert into memory only database")
-  }
+	db := open(t)
+  db.create(t, "CREATE TABLE test (col)")
+  db.insert(t, "INSERT INTO test VALUES ('value')")
   result, err := db.ExecFirstAsString("SELECT * FROM test")
-  if err != nil {
-    t.Error("Failed to select from test")
-  }
-  if result != "value" {
+  if err != nil || result != "value" {
     t.Error("Failed to exec first as string")
   }
 }
+
+func TestExecFirstAsInt(t *testing.T) {
+	db := open(t)
+  db.create(t, "CREATE TABLE test (col)")
+  db.insert(t, "INSERT INTO test VALUES (1)")
+  result, err := db.ExecFirstAsInt("SELECT * FROM test")
+  if err != nil || result != 1 {
+    t.Error("Failed to exec first as int")
+  }
+}
+
