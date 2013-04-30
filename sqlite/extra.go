07c045dbefa40c6aab19ec59d7dae2098e834802
Richard Lyman <richard.lyman@gmail.com>
2013-04-29 19:02:42 -0600

    .

--- sqlite/extra.go
+++ sqlite/extra.go
@@ -21,114 +21,113 @@ func (c *Conn) DropAllTables() {
 }
 */
 
-func Columns( stmnt *Stmt ) int {
-    return int(C.sqlite3_column_count(stmnt.stmt))
+func Columns(stmnt *Stmt) int {
+	return int(C.sqlite3_column_count(stmnt.stmt))
 }
 
 // ScanAllAsString will return all rows if error is nil.
 // If error is not nil, the rows successfully scanned up to
 // that point are the only rows returned.
-func ScanAllAsString( stmnt *Stmt ) ([][]string, error) {
-    result := [][]string{}
-    for stmnt.Next() {
-        row, err := ScanAsString(stmnt)
-        if err != nil {
-            return result, err
-        }
-        result = append(result, row)
-    }
-    return result, nil
+func ScanAllAsString(stmnt *Stmt) ([][]string, error) {
+	result := [][]string{}
+	for stmnt.Next() {
+		row, err := ScanAsString(stmnt)
+		if err != nil {
+			return result, err
+		}
+		result = append(result, row)
+	}
+	return result, nil
 }
 
-func ScanAsString( stmnt *Stmt ) ([]string, error) {
-    result := make([]string,Columns(stmnt))
-    addrs := make([]interface{},Columns(stmnt))
-    for i := range result {
-        addrs[i] = &result[i]
-    }
-    err := stmnt.Scan(addrs...)
-    if err != nil {
-        return nil, err
-    }
-    return result, nil
+func ScanAsString(stmnt *Stmt) ([]string, error) {
+	result := make([]string, Columns(stmnt))
+	addrs := make([]interface{}, Columns(stmnt))
+	for i := range result {
+		addrs[i] = &result[i]
+	}
+	err := stmnt.Scan(addrs...)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
 
-func ScanAsInt( stmnt *Stmt ) ([]int, error) {
-    result := make([]int,Columns(stmnt))
-    addrs := make([]interface{},Columns(stmnt))
-    for i := range result {
-        addrs[i] = &result[i]
-    }
-    err := stmnt.Scan(addrs...)
-    if err != nil {
-        return nil, err
-    }
-    return result, nil
+func ScanAsInt(stmnt *Stmt) ([]int, error) {
+	result := make([]int, Columns(stmnt))
+	addrs := make([]interface{}, Columns(stmnt))
+	for i := range result {
+		addrs[i] = &result[i]
+	}
+	err := stmnt.Scan(addrs...)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
 
 // See ScanAllAsString for an explanation of how possible errors
 // affect the rows returned
-func (c *Conn) ExecToStrings( sql string ) ([][]string, error) {
-    stmnt, prep_err := c.Prepare(sql)
-    if prep_err != nil {
-        return nil, prep_err
-    }
-    defer stmnt.Finalize()
-    has_rows := stmnt.Next()
-    if !has_rows {
-        return [][]string{}, nil
-    }
-    stmnt.Reset()
-    return ScanAllAsString(stmnt)
+func (c *Conn) ExecToStrings(sql string) ([][]string, error) {
+	stmnt, prep_err := c.Prepare(sql)
+	if prep_err != nil {
+		return nil, prep_err
+	}
+	defer stmnt.Finalize()
+	has_rows := stmnt.Next()
+	if !has_rows {
+		return [][]string{}, nil
+	}
+	stmnt.Reset()
+	return ScanAllAsString(stmnt)
 }
 
-func (c *Conn) SafeExecToStrings( sql string ) ([][]string, error) {
-  begin_err := c.Exec("BEGIN;")
-  if begin_err != nil {
-    return nil, errors.New(fmt.Sprintf("Failed to begin safe execution: %s", begin_err))
-  }
-  result, result_err := c.ExecToStrings(sql)
-  if result_err != nil {
-    return nil, errors.New(fmt.Sprintf("Failed to execute query: %s", result_err))
-  }
-  rollback_err := c.Exec("ROLLBACK;")
-  if rollback_err != nil {
-    return nil, errors.New(fmt.Sprintf("Failed to rollback safe execution: %s", rollback_err))
-  }
-  return result, nil
+func (c *Conn) SafeExecToStrings(sql string) ([][]string, error) {
+	begin_err := c.Exec("BEGIN;")
+	if begin_err != nil {
+		return nil, errors.New(fmt.Sprintf("Failed to begin safe execution: %s", begin_err))
+	}
+	result, result_err := c.ExecToStrings(sql)
+	if result_err != nil {
+		return nil, errors.New(fmt.Sprintf("Failed to execute query: %s", result_err))
+	}
+	rollback_err := c.Exec("ROLLBACK;")
+	if rollback_err != nil {
+		return nil, errors.New(fmt.Sprintf("Failed to rollback safe execution: %s", rollback_err))
+	}
+	return result, nil
 }
 
-func (c *Conn) ExecFirstAsString( sql string ) (string, error) {
-    stmnt, prep_err := c.Prepare(sql)
-    if prep_err != nil {
-        return "", prep_err
-    }
-    defer stmnt.Finalize()
-    has_rows := stmnt.Next()
-    if !has_rows {
-        return "", errors.New("There are no rows in the result set")
-    }
-    result, scan_err := ScanAsString(stmnt)
-    if scan_err != nil {
-        return "", scan_err
-    }
-    return result[0], nil
+func (c *Conn) ExecFirstAsString(sql string) (string, error) {
+	stmnt, prep_err := c.Prepare(sql)
+	if prep_err != nil {
+		return "", prep_err
+	}
+	defer stmnt.Finalize()
+	has_rows := stmnt.Next()
+	if !has_rows {
+		return "", errors.New("There are no rows in the result set")
+	}
+	result, scan_err := ScanAsString(stmnt)
+	if scan_err != nil {
+		return "", scan_err
+	}
+	return result[0], nil
 }
 
-func (c *Conn) ExecFirstAsInt( sql string ) (int, error) {
-    stmnt, prep_err := c.Prepare(sql)
-    if prep_err != nil {
-        return 0, prep_err
-    }
-    defer stmnt.Finalize()
-    has_rows := stmnt.Next()
-    if !has_rows {
-        return 0, errors.New("There are no rows in the result set")
-    }
-    result, scan_err := ScanAsInt(stmnt)
-    if scan_err != nil {
-        return 0, scan_err
-    }
-    return result[0], nil
+func (c *Conn) ExecFirstAsInt(sql string) (int, error) {
+	stmnt, prep_err := c.Prepare(sql)
+	if prep_err != nil {
+		return 0, prep_err
+	}
+	defer stmnt.Finalize()
+	has_rows := stmnt.Next()
+	if !has_rows {
+		return 0, errors.New("There are no rows in the result set")
+	}
+	result, scan_err := ScanAsInt(stmnt)
+	if scan_err != nil {
+		return 0, scan_err
+	}
+	return result[0], nil
 }
-
--- sqlite/sqlite_test.go
+++ sqlite/sqlite_test.go
@@ -1,90 +1,90 @@
 package sqlite
 
 import (
+	"fmt"
 	"testing"
-  "fmt"
 )
 
 func open(t *testing.T) *Conn {
-  db, err := Open(":memory:")
-  if err != nil {
-    t.Error("Failed to open memory database")
-  }
-  return db
+	db, err := Open(":memory:")
+	if err != nil {
+		t.Error("Failed to open memory database")
+	}
+	return db
 }
 
 func (db *Conn) create(t *testing.T, sql string) {
-  err := db.Exec(sql)
-  if err != nil {
-    t.Error("Failed to create")
-  }
+	err := db.Exec(sql)
+	if err != nil {
+		t.Error("Failed to create")
+	}
 }
 
 func (db *Conn) insert(t *testing.T, sql string) {
-  err := db.Exec(sql)
-  if err != nil {
-    t.Error("Failed to insert")
-  }
+	err := db.Exec(sql)
+	if err != nil {
+		t.Error("Failed to insert")
+	}
 }
 
 func TestExecFirstAsString(t *testing.T) {
 	db := open(t)
-  db.create(t, "CREATE TABLE test (col)")
-  db.insert(t, "INSERT INTO test VALUES ('value')")
-  result, err := db.ExecFirstAsString("SELECT * FROM test")
-  if err != nil || result != "value" {
-    t.Error("Failed to exec first as string")
-  }
+	db.create(t, "CREATE TABLE test (col)")
+	db.insert(t, "INSERT INTO test VALUES ('value')")
+	result, err := db.ExecFirstAsString("SELECT * FROM test")
+	if err != nil || result != "value" {
+		t.Error("Failed to exec first as string")
+	}
 }
 
 func TestExecFirstAsInt(t *testing.T) {
 	db := open(t)
-  db.create(t, "CREATE TABLE test (col)")
-  db.insert(t, "INSERT INTO test VALUES (1)")
-  result, err := db.ExecFirstAsInt("SELECT * FROM test")
-  if err != nil || result != 1 {
-    t.Error("Failed to exec first as int")
-  }
+	db.create(t, "CREATE TABLE test (col)")
+	db.insert(t, "INSERT INTO test VALUES (1)")
+	result, err := db.ExecFirstAsInt("SELECT * FROM test")
+	if err != nil || result != 1 {
+		t.Error("Failed to exec first as int")
+	}
 }
 
 func TestFinalizeNilStmnt(t *testing.T) {
-  var stmnt *Stmt = nil
-  err := stmnt.Finalize()
-  if err == nil || err.Error() != "Finalize called on nil Statement" {
-    t.Error("Failed to error or provide a useful error message when Finalize was called on a nil Statement")
-  }
+	var stmnt *Stmt = nil
+	err := stmnt.Finalize()
+	if err == nil || err.Error() != "Finalize called on nil Statement" {
+		t.Error("Failed to error or provide a useful error message when Finalize was called on a nil Statement")
+	}
 }
 
 func TestSQLNilStmnt(t *testing.T) {
-  defer func(){
-    if r := recover(); r == nil || r != "SQL called on nil Statement" {
-      t.Error("Failed to panic or provide a useful error message when SQL was called on nil Statement")
-    }
-  }()
-  var stmnt *Stmt = nil
-  stmnt.SQL()
+	defer func() {
+		if r := recover(); r == nil || r != "SQL called on nil Statement" {
+			t.Error("Failed to panic or provide a useful error message when SQL was called on nil Statement")
+		}
+	}()
+	var stmnt *Stmt = nil
+	stmnt.SQL()
 }
 
 func TestNanosecondsNilStmnt(t *testing.T) {
-  defer func(){
-    if r := recover(); r == nil || r != "Nanoseconds called on nil Statement" {
-      t.Error("Failed to panic or provide a useful error message when Nanoseconds was called on nil Statement")
-    }
-  }()
-  var stmnt *Stmt
-  stmnt.Nanoseconds()
+	defer func() {
+		if r := recover(); r == nil || r != "Nanoseconds called on nil Statement" {
+			t.Error("Failed to panic or provide a useful error message when Nanoseconds was called on nil Statement")
+		}
+	}()
+	var stmnt *Stmt
+	stmnt.Nanoseconds()
 }
 
 func TestSafeExecToStrings(t *testing.T) {
-  db := open(t)
-  db.create(t, "CREATE TABLE test(col)")
-  db.insert(t, "INSERT INTO test VALUES ('works')")
-  _, delete_err := db.SafeExecToStrings("DELETE FROM test")
-  if delete_err != nil {
-    t.Error(fmt.Sprintf("Failed to safely execute delete: %s", delete_err))
-  }
-  result, err := db.ExecToStrings("SELECT * FROM test")
-  if err != nil || result[0][0] != "works" {
-    t.Error(fmt.Sprintf("Failed to execute select after safely executing delete: %s", err))
-  }
+	db := open(t)
+	db.create(t, "CREATE TABLE test(col)")
+	db.insert(t, "INSERT INTO test VALUES ('works')")
+	_, delete_err := db.SafeExecToStrings("DELETE FROM test")
+	if delete_err != nil {
+		t.Error(fmt.Sprintf("Failed to safely execute delete: %s", delete_err))
+	}
+	result, err := db.ExecToStrings("SELECT * FROM test")
+	if err != nil || result[0][0] != "works" {
+		t.Error(fmt.Sprintf("Failed to execute select after safely executing delete: %s", err))
+	}
 }
